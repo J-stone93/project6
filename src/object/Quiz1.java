ae0d4f0661e31e2f0adddc33634350bfea24b2f1
jang <treous11@gmail.com>
2024-04-04 12:42:58 +0900

    init

---
+++ src/object/Quiz1.java
@@ -0,0 +1,29 @@
+package object;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student("둘리", 5000);
+		
+		Bus bus = new Bus(100);
+		
+		student1.takeBus(bus);
+		
+		student1.showInfo();
+		
+		bus.showInfo();
+		
+		Student student2 = new Student("도우너", 10000);
+		
+		Subway subway = new Subway("2호선");
+		
+		student2.takeSubway(subway);
+		
+		student2.showInfo();
+		
+		subway.showInfo();
+
+	}
+
+}
